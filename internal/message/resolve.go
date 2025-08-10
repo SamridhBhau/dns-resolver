bb18d3533823917c5fed3bc4653cd741918b7375
sam <sb.samridhbhau@gmail.com>
2025-08-10 23:13:18 +0530

    Added Resolve method

---
+++ internal/message/resolve.go
@@ -0,0 +1,93 @@
+package message
+
+import (
+	"fmt"
+)
+
+const RootNameServer = "192.33.4.12:53"
+
+func (m Message) Resolve(address string) (string, error) {
+	fmt.Printf("Querying " + address + " for " + m.Q.QName + "\n")
+
+	recvBytes, err := m.SendRequest(address)
+
+	if err != nil {
+		fmt.Println("Send request failed")
+		return "", err
+	}
+
+	recvMsg := ParseResponse(recvBytes)
+
+	if recvMsg.H.ANCOUNT > 0 {
+		answer := recvMsg.ANS[0]
+		return answer.RData, nil
+	}
+	if recvMsg.H.NSCOUNT > 0 {
+		var found bool
+		// Search for Type 2 Record
+		nameServerIP := ""
+		for _, ns := range recvMsg.AUTH {
+			if ns.Type == 2 {
+				for _, ad := range recvMsg.ADD {
+					if ad.Name == ns.RData {
+						found = true
+						nameServerIP = ad.RData
+						break
+					}
+				}
+			}
+		}
+
+		if found {
+			res, err := m.Resolve(nameServerIP + ":53")
+
+			if err != nil {
+				return "", err
+			}
+
+			return res, nil
+
+		} else {
+			var nameServer ResourceRecord
+			var found bool
+			for _, ns := range recvMsg.AUTH {
+				if ns.Type == 2 {
+					nameServer = ns
+					found = true
+					break
+				}
+			}
+
+			if found {
+				newMsg := Message{
+					H: Header{
+						ID:      22,
+						QDCOUNT: 1,
+					},
+					Q: Question{
+						nameServer.RData,
+						1,
+						1,
+					},
+				}
+
+				nameServerIP, err := newMsg.Resolve(RootNameServer)
+
+				if err != nil {
+					fmt.Println("Failed to resolve: " + nameServer.RData)
+					return "", nil
+				}
+
+				res, err := m.Resolve(nameServerIP + ":53")
+
+				if err != nil {
+					fmt.Println("Failed to resolve: " + m.Q.QName)
+					return "", nil
+				}
+
+				return res, nil
+			}
+		}
+	}
+	return "", fmt.Errorf("not found")
+}
--- main.go
+++ main.go
@@ -2,24 +2,21 @@ package main
 
 import (
 	"fmt"
-	"net"
 	"os"
 
 	"github.com/SamridhBhau/dnsResolver/internal/message"
-	"github.com/SamridhBhau/dnsResolver/internal/parse"
 )
 
-const RootNameServer = "198.41.0.4:53"
+const RootNameServer = "192.33.4.12:53"
 
 func main() {
 	header := message.Header{
 		ID:      22,
-		RD:      true,
 		QDCOUNT: 1,
 	}
 
 	question := message.Question{
-		QName:  "go.dev",
+		QName:  "news.ycombinator.com",
 		QType:  1,
 		QClass: 1,
 	}
@@ -29,37 +26,9 @@ func main() {
 		Q: question,
 	}
 
-	msgBytes := msg.Marshal()
-
-	udpAddr, err := net.ResolveUDPAddr("udp", RootNameServer)
+	ip, err := msg.Resolve(RootNameServer)
 	if err != nil {
-		fmt.Println("ResolveUDPAddr error", err.Error())
 		os.Exit(1)
 	}
-
-	conn, err := net.DialUDP("udp", nil, udpAddr)
-	if err != nil {
-		fmt.Println("Listen failed: ", err.Error())
-		os.Exit(1)
-	}
-
-	defer conn.Close()
-
-	_, err = conn.Write(msgBytes)
-
-	if err != nil {
-		fmt.Println("Write failed: ", err.Error())
-		os.Exit(1)
-	}
-
-	recvBuf := make([]byte, message.UDPMAXSIZE)
-	_, err = conn.Read(recvBuf)
-
-	if err != nil {
-		fmt.Println("Read failed: ", err.Error())
-		os.Exit(1)
-	}
-
-	resMsg := parse.ParseResponse(recvBuf)
-	resMsg.Display()
+	fmt.Println("IP address is: " + ip)
 }
